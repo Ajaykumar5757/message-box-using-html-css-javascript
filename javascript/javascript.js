be9783fb42311a62dc020fa6ea994ac6751333c4
Ajaykumar A <ajaykumarak2703@gmail.com>
2024-07-23 19:45:44 +0530

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,17 +1,33 @@
-let msg=document.getElementById("text");
+let ques=["how was your day?","what is html?","why we use html?",
+    "what is the full form of css?","what javascript does in web design?",
+    "can we use bootstrap?","can media query is mandatory?",
+    "what if the style is not done for web page?","can the web page always dynamic?"]
+    
 function check(){
-    if(msg=='how was your day?'){
+    let msg=document.getElementById("text").value;
+
+    if(ques.includes(msg)){
         document.getElementById("content").innerHTML="your question is accepted";
         document.getElementsByClassName("btn1")[0].style.backgroundColor="green";
         document.getElementsByClassName("btn1")[0].style.color="white";
-        document.getElementsByClassName("btn2")[0].style.backgroundColor="gold";
-        document.getElementsByClassName("btn2")[0].style.color="black";
     }
     else{
-        document.getElementById("content").innerHTML="your question is declined";
-        document.getElementsByClassName("btn2")[0].style.backgroundColor="red";
-        document.getElementsByClassName("btn2")[0].style.color="white";
-        document.getElementsByClassName("btn1")[0].style.backgroundColor="gold";
-        document.getElementsByClassName("btn1")[0].style.color="black";
+        if(msg==""){
+            alert("ask the question properly");
+        }
+        else{
+        document.getElementById("content").innerHTML="your question is declined <br>try to give correct question to get accepted";
+        document.getElementsByClassName("btn1")[0].style.backgroundColor="red";
+        document.getElementsByClassName("btn1")[0].style.color="white";
+        }
     }
+}
+
+function decline(){
+    document.getElementsByClassName("btn1")[0].style.backgroundColor="gold";
+    document.getElementsByClassName("btn1")[0].style.color="black";
+    document.getElementsByClassName("btn2")[0].style.backgroundColor="gold";
+    document.getElementsByClassName("btn2")[0].style.color="black";
+    document.getElementById("content").innerHTML="";
+    // document.getElementById("text").reset();
 }
