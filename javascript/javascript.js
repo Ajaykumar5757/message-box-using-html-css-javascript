a2a06bacb6d0340d0cc1cd9228e4e1c8b3b0d9d4
Ajaykumar A <ajaykumarak2703@gmail.com>
2024-07-23 17:58:59 +0530

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,17 @@
+let msg=document.getElementById("text").value;
+function check(){
+    if(msg=='how was your day?'){
+        document.getElementById("content").innerHTML="your question is accepted";
+        document.getElementsByClassName("btn1")[0].style.backgroundColor="green";
+        document.getElementsByClassName("btn1")[0].style.color="white";
+        document.getElementsByClassName("btn2")[0].style.backgroundColor="gold";
+        document.getElementsByClassName("btn2")[0].style.color="black";
+    }
+    else{
+        document.getElementById("content").innerHTML="your question is declined";
+        document.getElementsByClassName("btn2")[0].style.backgroundColor="red";
+        document.getElementsByClassName("btn2")[0].style.color="white";
+        document.getElementsByClassName("btn1")[0].style.backgroundColor="gold";
+        document.getElementsByClassName("btn1")[0].style.color="black";
+    }
+}
